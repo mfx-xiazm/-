cc97fac65204c213c73e12580d1e8a7086f67002
xiazm <1129483989@qq.com>
2019-09-05 17:11:18 +0800

    调整资讯详情的分享和收藏布局

--- XFT-机构版/XFT/Class/News-资讯/Controller/RCNewsDetailVC.m
+++ XFT-机构版/XFT/Class/News-资讯/Controller/RCNewsDetailVC.m
@@ -19,33 +19,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.navigationItem setTitle:@"资讯详情"];
+    [self setUpNavBar];
 }
-- (IBAction)detailBtnClicked:(SPButton *)sender {
-    if (sender.tag == 1) {
-        HXLog(@"收藏");
-    }else{
-        RCShareView *share = [RCShareView loadXibView];
-        share.hxn_width = HX_SCREEN_WIDTH;
-        share.hxn_height = 260.f;
-        hx_weakify(self);
-        share.shareTypeCall = ^(NSInteger type, NSInteger index) {
-            if (type == 1) {
-                if (index == 1) {
-                    HXLog(@"微信好友");
-                }else if (index == 2) {
-                    HXLog(@"朋友圈");
-                }else{
-                    HXLog(@"链接");
-                }
-            }else{
-                [weakSelf.zh_popupController dismissWithDuration:0.25 springAnimated:NO];
-            }
-        };
-        self.zh_popupController = [[zhPopupController alloc] init];
-        self.zh_popupController.layoutType = zhPopupLayoutTypeBottom;
-        [self.zh_popupController presentContentView:share duration:0.25 springAnimated:NO];
-    }
-}
+-(void)setUpNavBar
+{
+    UIBarButtonItem *shareItem = [UIBarButtonItem itemWithTarget:self action:@selector(shareClicked) nomalImage:HXGetImage(@"搜索") higeLightedImage:HXGetImage(@"搜索") imageEdgeInsets:UIEdgeInsetsZero];
+    UIBarButtonItem *collectItem = [UIBarButtonItem itemWithTarget:self action:@selector(collectClicked) nomalImage:HXGetImage(@"搜索") higeLightedImage:HXGetImage(@"搜索") imageEdgeInsets:UIEdgeInsetsZero];
 
+    self.navigationItem.rightBarButtonItems = @[shareItem,collectItem];
+
+}
+#pragma mark -- 点击事件
+-(void)shareClicked
+{
+    RCShareView *share = [RCShareView loadXibView];
+    share.hxn_width = HX_SCREEN_WIDTH;
+    share.hxn_height = 260.f;
+    hx_weakify(self);
+    share.shareTypeCall = ^(NSInteger type, NSInteger index) {
+        if (type == 1) {
+            if (index == 1) {
+                HXLog(@"微信好友");
+            }else if (index == 2) {
+                HXLog(@"朋友圈");
+            }else{
+                HXLog(@"链接");
+            }
+        }else{
+            [weakSelf.zh_popupController dismissWithDuration:0.25 springAnimated:NO];
+        }
+    };
+    self.zh_popupController = [[zhPopupController alloc] init];
+    self.zh_popupController.layoutType = zhPopupLayoutTypeBottom;
+    [self.zh_popupController presentContentView:share duration:0.25 springAnimated:NO];
+}
+-(void)collectClicked
+{
+    HXLog(@"收藏");
+
+}
 
 @end
